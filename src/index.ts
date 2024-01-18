db2133c32a3f288f7604ede5560ce95257c93a43
Arvind Prajapati <arvind.prajapati20@gmail.com>
2024-01-18 14:01:06 +0530

    added optional parameters

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,21 @@ app.get('/api/v1/bodies', async (req: Request, res: Response) => {
 
 //API Ninjas Cars API endpoint.
 //Get car data from given parameters. Returns a list of car models (and their information) that satisfy the parameters.
+// Parameters
+// make (optional) - vehicle manufacturer (e.g. audi or toyota).
+// model (optional) - vehicle manufacturer (e.g. a4 or corolla).
+// fuel_type (optional) - type of fuel used. Possible values: gas, diesel, electricity.
+// drive (optional) - drive transmission. Possible values: fwd (front-wheel drive), rwd (rear-wheel drive), awd (all-wheel drive), 4wd (four-wheel drive).
+// cylinders (optional) - number of cylinders in engine. Possible values: 2, 3 4, 5, 6, 8, 10, 12, 16.
+// transmission (optional) - type of transmission. Possible values: manual, automatic.
+// year (optional) - vehicle model year (e.g. 2018).
+// min_city_mpg (optional) - minimum city fuel consumption (in miles per gallon).
+// max_city_mpg (optional) - maximum city fuel consumption (in miles per gallon).
+// min_hwy_mpg (optional) - minimum highway fuel consumption (in miles per gallon).
+// max_hwy_mpg (optional) - maximum highway fuel consumption (in miles per gallon).
+// min_comb_mpg (optional) - minimum combination (city and highway) fuel consumption (in miles per gallon).
+// max_comb_mpg (optional) - maximum combination (city and highway) fuel consumption (in miles per gallon).
+// limit (optional) - How many results to return. Must be between 1 and 50. Default is 5.
 app.get('/api/v1/cars', async (req: Request, res: Response) => {
     let params = req.body || { model: 'corolla' };
     const options = {
